6b376beeb02e8d0477da7b6e3e11ffccc10d1006
YasminLN12 <46935028+YasminLN12@users.noreply.github.com>
2019-04-23 17:47:23 -0500

    Yasmín Landaverde Nava, Grupo 03
    Hice lo que pude con el extra :(

---
+++ Misión_08.py
@@ -0,0 +1,145 @@
+# Autor: Yasmín Landaverde Nava
+# Descripción: ejercicios de cadenas
+
+# EJERCICIO NO.1
+def combinarLetras(cadena):
+    combinacion = ""
+    for i in range(0, len(cadena), 1):
+        letra = cadena[i]
+        if i % 2 == 0:
+            combinacion += letra.upper()
+        else:
+            combinacion += letra.lower()
+    return combinacion
+
+
+# EJERCICIO NO.2
+def contieneLasVocales(palabra):
+    palabraCorrecta = palabra.lower()
+    vocales = 0
+    if "a" in palabraCorrecta:
+        vocales += 1
+    if "e" in palabraCorrecta:
+        vocales += 1
+    if "i" in palabraCorrecta:
+        vocales += 1
+    if "o" in palabraCorrecta:
+        vocales += 1
+    if "u" in palabraCorrecta:
+        vocales += 1
+
+    if vocales == 5:
+        return True
+    else:
+        return False
+
+# EJERCICIO NO.3
+def formarNombreUsuario(nombre, apellido, matricula):
+    nombre3 = nombre.lower()[0:3]
+    apellido3 = apellido.lower()[0:3]
+    matricula3 = matricula[4:8]
+    return nombre3 + apellido3 + matricula3
+
+# ERJERCICIO NO.4
+def esCorrecto(nombre):
+    nombre = nombre.split()
+    for palabra in nombre:
+        for letra in palabra[0]:
+            if letra.islower():
+                return False
+        for letra in palabra[1::]:
+            if letra.isupper():
+                return False
+    return True
+
+# EJERCICIO NO. 5
+def traducirTelefono(numero):
+    marcacion= numero.split("-")
+    palabraCero = marcacion[0]
+    palabraUno = marcacion[1]
+    palabraDos = marcacion[2]
+    digito = ""
+
+    for valor in palabraUno:
+        if valor == "A" or valor == "B" or valor == "C":
+            digito = digito + "2"
+        if valor == "D" or valor == "E"  or valor == "F":
+            digito = digito + "3"
+        if valor == "G" or valor == "H" or valor == "I":
+            digito = digito + "4"
+        if valor == "J" or valor == "K" or valor == "L":
+            digito = digito + "5"
+        if valor == "M" or valor == "N" or valor == "O":
+            digito = digito + "6"
+        if valor == "P" or valor == "Q" or valor == "R" or valor == "S":
+            digito = digito + "7"
+        if valor == "T" or valor == "U" or valor == "V":
+            digito = digito + "8"
+        if valor == "W" or valor == "X" or valor == "Y" or valor == "Z":
+            digito = digito + "9"
+
+    digitoDos = ""
+    for valor in palabraDos:
+        if valor == "A" or valor == "B" or valor == "C":
+            digitoDos = digitoDos + "2"
+        if valor == "D" or valor == "E"  or valor == "F":
+            digitoDos = digitoDos + "3"
+        if valor == "G" or valor == "H" or valor == "I":
+            digitoDos = digitoDos + "4"
+        if valor == "J" or valor == "K" or valor == "L":
+            digitoDos = digitoDos + "5"
+        if valor == "M" or valor == "N" or valor == "O":
+            digitoDos = digitoDos + "6"
+        if valor == "P" or valor == "Q" or valor == "R" or valor == "S":
+            digitoDos = digitoDos + "7"
+        if valor == "T" or valor == "U" or valor == "V":
+            digitoDos = digitoDos + "8"
+        if valor == "W" or valor == "X" or valor == "Y" or valor == "Z":
+            digitoDos = digitoDos + "9"
+
+
+    print (palabraCero, "-", digito, "-", digitoDos)
+
+
+# EEJRCICIO EXTRA
+def esValido(contrasena):
+    validaciones = 0
+
+    if len(contrasena) >= 8:
+        validaciones += 1
+
+    if not contrasena.isalnum():
+        validaciones += 1
+
+    if validaciones == 2:
+        print ("es valido")
+    else:
+        print ("no")
+
+
+def main():
+    frase = "hola  MUNDO"
+    resultado = combinarLetras(frase)
+    print (resultado)
+
+    palabra = "abuelito"
+    vocales = contieneLasVocales(palabra)
+    print (vocales)
+
+    nombre = "Yasmín"
+    apellido = "Landaverde"
+    matricula = "1745725"
+    usuario = formarNombreUsuario(nombre, apellido, matricula)
+    print(usuario)
+
+    nombre = "Yasmin Landaverde Nava"
+    error = esCorrecto(nombre)
+    print (error)
+
+    numero = "01800-VOY-BIEN"
+    traducirTelefono(numero)
+
+    contrasena = "asafgonweoi!AA"
+    esValido(contrasena)
+
+main()
