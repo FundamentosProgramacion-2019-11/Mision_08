3d116045b13d077228e9d6a2cca13ab8520ac0d1
FrancisJaGlez <46821629+FrancisJaGlez@users.noreply.github.com>
2019-04-29 21:13:20 -0500

    FRANCISCO JAVIER GONZALEZ MOLINA, GRUPO 03
    Profesor, no se si pueda checar la segunda funcion, estoy seguro que esta bien, pero me la marca mal el programa, no se si pudiera checarla y pudiera aclarar mi duda, Buen dia prof.

--- Misión_08.py
+++ Misión_08.py
@@ -8,16 +8,22 @@ def combinarLetras (cadena):
     return minMay
 
 def contieneLasVocales(cadena2):
-    if ("a"or "A") and ("e"or"E") and ("i"or"I") and ("o"or"O") and ("u"or"U") in cadena2:
-        return True
-    else:
-        return False
+    if ("a" or "A") in cadena2:
+        if("e" or "E")in cadena2:
+            if("i" or "I")in cadena2:
+                if("o" or "O") in cadena2:
+                    if("u" or "U") in cadena2:
+                        return True
+    return False
 
-def formarnombreUsuario(nombre,apellidoPaterno,matricula):
-    nombre3= nombre[0:3]
-    apellido3= apellidoPaterno[0:3]
-    if len(matricula) == 7:
-        matstr= str(matricula)
+
+def formarNombreUsuario(nombre,apellido,matricula):
+    minusculas= nombre.lower()
+    minusculasApe= apellido.lower()
+    nombre3= minusculas[0:3]
+    apellido3= minusculasApe[0:3]
+    matstr = str(matricula)
+    if len(matstr) == 7:
         matricula3= matstr[-3:]
         return (nombre3 + apellido3 + matricula3)
     else:
@@ -25,40 +31,41 @@ def formarnombreUsuario(nombre,apellidoPaterno,matricula):
 
 def esCorrecto (cadena3):
     cadenastr= cadena3.split()
-    print (cadenastr)
-    for letra in cadenastr:
-        mayus= letra[0:1]
-        minus=  letra[1:]
-        if mayus.isupper() and minus.islower:
-            return True
-        else:
-            return False
+    palabra= cadenastr[0]
+    palabra2= cadenastr[1]
+    palabra3= cadenastr[2]
+    if palabra[0].isupper() and palabra[1:].islower():
+        if palabra2[0].isupper() and palabra2[1:].islower():
+            if palabra3[0].isupper() and palabra3[1:].islower():
+                return  True
+
+    return False
 
 def traducirTelefono(numeroTelefono):
     digito=""
-    for numero in (numeroTelefono):
+    mensaje= numeroTelefono[6::]
+    for numero in mensaje:
         if numero == "1":
             digito +="1"
-        elif numero == "A" or numero=="B"or numero== "C":
+        elif (numero == "A") or (numero == "B") or (numero== "C"):
             digito +=  "2"
-        elif numero == "D" or numero=="E" or numero=="F":
+        elif (numero == "D" )or (numero == "E") or (numero=="F"):
             digito +=  "3"
-        elif numero == "G" or numero== "H" or numero== "I":
+        elif (numero == "G") or (numero == "H") or (numero== "I"):
             digito += "4"
-        elif numero == numero=="J" or numero=="K"or numero=="L":
+        elif (numero == "J") or (numero == "K") or (numero=="L"):
             digito +=  "5"
-        elif numero == "M" or numero=="N"or numero=="O":
+        elif (numero == "M") or (numero == "N") or (numero=="O"):
             digito +=  "6"
-        elif numero == "P" or numero=="Q"or numero=="R" or numero=="S":
+        elif (numero == "P") or (numero == "Q") or (numero=="R") or (numero=="S"):
             digito +=  "7"
-        elif numero == "T"or numero== "U" or numero=="V":
+        elif (numero == "T") or (numero == "U") or (numero=="V"):
             digito +=  "8"
-        elif numero == "W" or numero== "X" or numero=="Y" or numero=="Z":
+        elif (numero == "W") or (numero == "X") or (numero=="Y") or (numero=="Z"):
             digito +=  "9"
         else:
-            digito +=  "-"
-        formato= "01800"+digito
-        return formato
+            digito += "-"
+    return "01800-"+digito
 
 
 def main():
