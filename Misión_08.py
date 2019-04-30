e4ee203261896096374781af2a2a0ad651e2eb51
LuisCarmonaV <46898166+LuisCarmonaV@users.noreply.github.com>
2019-04-29 19:45:42 -0500

    mision 8 corregida
    Luis Adrian Carmona 
    A01748395

--- Misión_08.py
+++ Misión_08.py
@@ -1,6 +1,9 @@
 #Autor : Luis Adrian Carmona Villalobos
 #Mision 8
 
+
+
+
 def combinarLetras(cadena):
     num=len(cadena)
     a = ""
@@ -14,45 +17,63 @@ def combinarLetras(cadena):
     return a
 
 
-def contieneLasVocales(cadena2): #detecta si la palabra tiene todas las vocales
-    vocales = "AEIOUaeiou"
-    newcadena2 = ""
-    for i in cadena2:
-        if i in vocales:
-            newcadena2 = newcadena2 +cadena2
-            print(True)
-            return newcadena2
-        else:
-            newcadena2 = newcadena2 +cadena2
-            print(False)
-            return  newcadena2
+def contieneLasVocales(cadena2):  # detecta si la palabra tiene todas las vocales
+    vocales = "aeiuoAEIOU"
+    soloVocales = ""
+    for letra in cadena2:
+        if letra in vocales:
+            soloVocales = soloVocales+letra.lower()
+    if "a" in soloVocales and "e" in soloVocales and "o" in soloVocales and "u" in soloVocales and "i" in soloVocales:
+        return True
+    else:
+        return False
+
+
+
 
 
 def formarNombreUsuario(nombre, apellido, matricula):
+    nombre = nombre.lower()[0:3]
+    apellido = apellido.lower()[0:3]
+    newapellido = apellido
     matriculastr = str(matricula)
-    newcadena3 = print(nombre.lower()[0:3]+apellido.lower()[0:3]+matriculastr[0:3])
-    return newcadena3
+    matriculastr = matriculastr[4:]
+    newnum = matriculastr
+
+    newnameform = nombre + newapellido + newnum
+    return newnameform
 
 
 def esCorrecto(cadena4):
     if cadena4 == cadena4.title():
-        print(True)
+        return True
     else:
-        print(False)
+        return False
 
 def traducirTelefono(numeroTel):
-    num =len(numeroTel)
-    acumulador = ""
-    for i in range(num):
-        if i == "1" or "2" or "3" or "4" or "5" or "0":
-            i = "1"
-            acumulador += i
-        elif num == "A" or "B" or "C":
-            num ="2"
-            acumulador = acumulador+ num
+    numero = numeroTel
+    a =""
+    for i in numero:
+        if i in "-0123456789":
+            a = a + i
+        elif i in "ABC":
+            a = a + "2"
+        elif i in "DEF":
+            a = a + "3"
+        elif i in "GHI":
+            a = a + "4"
+        elif i in "JKL":
+            a = a + "5"
+        elif i in "MNO":
+            a = a + "6"
+        elif i in "PQRS":
+            a = a + "7"
+        elif i in "TUV":
+            a = a + "8"
+        elif i in "WXYZ":
+            a = a + "9"
+    return a
 
-
-    return acumulador
 
 def main():
     cadena = input("Escribe la cadena que quieres alternar: ")
@@ -60,23 +81,23 @@ def main():
     nueva= combinarLetras(cadena)
     print(nueva)
 
-    cadena2 = input("Escribe una cadena: ")
-    contieneLasVocales(cadena2)
+
 
     nombre = input("Cual es tu nombre?: ")
     apellido = input("tu apellido paterno?: ")
-
     matricula = int(input("Su matricula pero solo los 7 digitos enteros: "))
-    formarNombreUsuario(nombre,apellido,matricula)
+    new3 = formarNombreUsuario(nombre, apellido, matricula)
+    print(new3)
+
 
     cadena4 = input("Cual es su nombre completo: ")
-    esCorrecto(cadena4)
+    new4 = esCorrecto(cadena4)
+    print(new4)
 
 
-    numeroTel = str(input("pon tu cadena: "))
+    numeroTel = str(input("pon el numero: "))
     traducirTelefono(numeroTel)
-    nueva = traducirTelefono(numeroTel)
-    print(nueva)
-
+    nueva5 = traducirTelefono(numeroTel)
+    print(nueva5)
 
 main()
